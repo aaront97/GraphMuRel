9104b9548f2cbc837cceef1f0f9b93e0fa6842a1
aaron.t97 <33289351+aaront97@users.noreply.github.com>
2019-10-28 02:02:32 +0000

    fixed abstractvqa

--- dataset/AbstractVQADataset.py
+++ dataset/AbstractVQADataset.py
@@ -4,34 +4,45 @@ import os
 import progressbar
 import json
 from collections import Counter
+import re
+import codecs
 
 class AbstractVQADataset(Dataset):
 	def __init__(self, 
 		processed_dir='data/processed_splits', 
 		model='baseline',
 		root_dir='D:/VQA/',
-		no_answers=1000,
+		no_answers=3000,
 		sample_answers=False,
 		skipthoughts_dir='data/skipthoughts'):
 
 		#Todo: Rationalise sampling answers?
+		self.no_answers = 3000
 		self.skipthoughts_dir = skipthoughts_dir
 		self.root_dir = root_dir
 		self.processed_dir = processed_dir
-		if not os.path.exists(processed_dir):
-			os.system('mkdir -p' + self.processed_dir)
+		if not os.path.exists(self.processed_dir):
 			self.process()
+		self.train_set = torch.load(os.path.join(self.processed_dir, 'train2014_processed.pth'))
+		self.val_set = torch.load(os.path.join(self.processed_dir, 'val2014_processed.pth'))
+		self.test_set = torch.load(os.path.join(self.processed_dir, 'testdev2015_processed.pth'))
+		self.wid_to_word = torch.load(os.path.join(self.processed_dir, 'wid_to_word.pth'))
+		self.word_to_wid = torch.load(os.path.join(self.processed_dir, 'word_to_wid.pth'))
+		self.ans_to_aid = torch.load(os.path.join(self.processed_dir, 'ans_to_aid.pth'))
+		self.aid_to_ans = torch.load(os.path.join(self.processed_dir, 'aid_to_ans.pth'))
 
 	def tokenize(self, s):
 		#we don't replace # because # is used to refer to number of items
+		s = s.rstrip()
 		t_str = s.lower()
-	    for i in [r'\?',r'\!',r'\'',r'\"',r'\$',r'\:',r'\@',r'\(',r'\)',r'\,',r'\.',r'\;']:
-	        t_str = re.sub( i, '', t_str)
-	    for i in [r'\-',r'\/']:
-	        t_str = re.sub( i, ' ', t_str)
-	    q_list = re.sub(r'\?','',t_str.lower()).split(' ')
-	    q_list = list(filter(lambda x: len(x) > 0, q_list))
-	    return q_list
+		for i in [r'\?',r'\!',r'\'',r'\"',r'\$',r'\:',r'\@',r'\(',r'\)',r'\,',r'\.',r'\;']:
+			t_str = re.sub( i, '', t_str)
+
+		for i in [r'\-',r'\/']:
+			t_str = re.sub( i, ' ', t_str)
+			q_list = re.sub(r'\?','',t_str.lower()).split(' ')
+			q_list = list(filter(lambda x: len(x) > 0, q_list))
+		return q_list
 
 	def merge_questions_with_annotations(self, ques, anno):
 		for q_key in ques:
@@ -56,8 +67,8 @@ class AbstractVQADataset(Dataset):
 		dictionary_path = os.path.join(self.skipthoughts_dir, 'dictionary.txt')
 		if not os.path.exists(dictionary_path):
 			os.system('wget {} -P {}'.format('http://www.cs.toronto.edu/~rkiros/models/dictionary.txt', self.skipthoughts_dir))
-		with open(dictionary_path, 'r') as f:
-			dict_list = f.readlines()
+		with open(dictionary_path, 'r', encoding='utf-8') as f:
+			dict_list = f.read().splitlines()
 		return set(dict_list)
 
 	def get_known_words(self, split_set):
@@ -67,15 +78,14 @@ class AbstractVQADataset(Dataset):
 		known_words_list = []
 		unknown_words_list = []
 		for question_dict in questions_list:
-			ques = question_dict['question']
-			for word in ques['question_tokens']:
+			for word in question_dict['question_tokens']:
 				if word in skipthoughts_dictionary:
 					known_words_list.append(word)
 				else:
 					unknown_words_list.append(word)
 		known_words_list.append('UNK')
 		print('No. of known words: {}, No. of unknown words : {}, Percentage Loss of words: {}%' \
-			.format(len(known_words), len(unknown_words), (len(unknown_words) / (len(known_words) + len(unknown_words))) * 100))
+			.format(len(known_words_list), len(unknown_words_list), (len(unknown_words_list) / (len(known_words_list) + len(unknown_words_list))) * 100))
 		return known_words_list, unknown_words_list
 
 	def add_most_frequent_answer(self, split_set):
@@ -89,9 +99,9 @@ class AbstractVQADataset(Dataset):
 		return split_set
 
 	def tokenize_questions(self, split_set):
-		print('Tokenizing questions for {}'.format(splist_set['data_subtype']))
-		for i in progressbar.progressbar(len(range(len(split_set['questions'])))):
-			question = split_set['question'][i]
+		print('Tokenizing questions for {}'.format(split_set['data_subtype']))
+		for i in progressbar.progressbar(range(len(split_set['questions']))):
+			question = split_set['questions'][i]
 			question['question_tokens'] = self.tokenize(question['question'])
 		return split_set
 
@@ -99,7 +109,7 @@ class AbstractVQADataset(Dataset):
 		questions_list = split_set['questions']
 		for question_dict in questions_list:
 			question_dict['question_tokens_UNK'] = \
-				[word if word in word_to_wid for word in question_dict['question_tokens']]
+				[word for word in question_dict['question_tokens'] if word in word_to_wid]
 		return split_set
 
 	def add_question_ids(self, split_set, word_to_wid):
@@ -112,7 +122,7 @@ class AbstractVQADataset(Dataset):
 		print('Removing questions if they have infrequent answers')
 		new_annotations = []
 		new_questions = []
-		if(len(split_set['questions'] != len(split_set['annotations']))):
+		if(len(split_set['questions']) != len(split_set['annotations'])):
 			raise ValueError()
 		for i in progressbar.progressbar(range(len(split_set['annotations']))):
 			annotation = split_set['annotations'][i]
@@ -140,21 +150,23 @@ class AbstractVQADataset(Dataset):
 
 
 	def process(self):
-	
+		path_train_anno = os.path.join(self.root_dir, 'Annotations', 'v2_mscoco_train2014_annotations.json')
+		path_val_anno = os.path.join(self.root_dir, 'Annotations', 'v2_mscoco_val2014_annotations.json')
+		path_train_ques = os.path.join(self.root_dir, 'Questions', 'v2_OpenEnded_mscoco_train2014_questions.json')
+		path_val_ques = os.path.join(self.root_dir, 'Questions', 'v2_OpenEnded_mscoco_val2014_questions.json')
+		path_test_ques = os.path.join(self.root_dir, 'Questions', 'v2_OpenEnded_mscoco_test-dev2015_questions.json')
 
-		path_train_anno = os.path.join(self.root_dir, 'Annotations', 'v2_mscoco_train2014_annotations')
-		path_val_anno = os.path.join(self.root_dir, 'Annotations', 'v2_mscoco_val2014_annotations')
-		path_train_ques = os.path.join(self.root_dir, 'Questions', 'v2_OpenEnded_mscoco_train2014_questions')
-		path_val_ques = os.path.join(self.root_dir, 'Questions', 'v2_OpenEnded_mscoco_val2014_questions')
-		path_test_ques = os.path.join(self.root_dir, 'Questions', 'v2_OpenEnded_mscoco_test2015_questions')
+		with open(path_train_ques, 'r') as train_ques_handle, open(path_train_anno, 'r') as train_anno_handle:
+			train_set = self.merge_questions_with_annotations(json.load(train_ques_handle), \
+				json.load(train_anno_handle))
 
-		train_set = self.merge_questions_with_annotations(json.load(path_train_ques),
-			json.load(open(path_train_anno)))
-		val_set = self.merge_questions_with_annotations(json.load(path_val_ques),
-			json.load(open(path_val_anno)))
+		with open(path_val_ques, 'r') as val_ques_handle, open(path_val_anno, 'r') as val_anno_handle:
+			val_set = self.merge_questions_with_annotations(json.load(val_ques_handle), \
+				json.load(val_anno_handle))
 
 		#We don't have annotations for the test set
-		test_set = json.load(open(path_test_ques))
+		with open(path_test_ques, 'r') as test_ques_handle:
+			test_set = json.load(test_ques_handle)
 
 		train_set = self.add_images(train_set)
 		val_set = self.add_images(val_set)
@@ -173,7 +185,7 @@ class AbstractVQADataset(Dataset):
 
 		known_words, unknown_words = self.get_known_words(train_set)
 		wid_to_word = {idx: word for idx, word in enumerate(known_words)}
-		word_to_wid = {word:idx} for idx, word in enumerate(known_words)}
+		word_to_wid = {word:idx for idx, word in enumerate(known_words)}
 
 		train_set = self.remove_question_if_not_top_answer(train_set, ans_to_aid)
 
@@ -186,16 +198,20 @@ class AbstractVQADataset(Dataset):
 		test_set = self.add_question_ids(test_set, word_to_wid)
 
 		train_set = self.train_encode_answers(train_set, ans_to_aid)
-		val_set = self.val_encode_answers(train_set, ans_to_aid)
+		val_set = self.val_encode_answers(val_set, ans_to_aid)
 
 		print('Saving processed datasets...')
 
 		if not os.path.exists(self.processed_dir):
-			os.system('mkdir -p ' + self.processed_dir)
+			os.system('mkdir "' + self.processed_dir + '"')
 
 		torch.save(train_set, os.path.join(self.processed_dir, 'train2014_processed.pth'))
 		torch.save(val_set, os.path.join(self.processed_dir, 'val2014_processed.pth'))
-		torch.save(test_set, os.path.join(self.processed_dir, 'test2015_processed.pth'))
+		torch.save(test_set, os.path.join(self.processed_dir, 'testdev2015_processed.pth'))
+		torch.save(wid_to_word, os.path.join(self.processed_dir, 'wid_to_word.pth'))
+		torch.save(word_to_wid, os.path.join(self.processed_dir, 'word_to_wid.pth'))
+		torch.save(ans_to_aid, os.path.join(self.processed_dir, 'ans_to_aid.pth'))
+		torch.save(aid_to_ans, os.path.join(self.processed_dir, 'aid_to_ans.pth'))
 
 		print('Finished processing annotations and questions.')
 
---
+++ dataset/ConcatBaselineDataset.py
@@ -0,0 +1,8 @@
+from . import AbstractVQADataset
+
+class ConcatBaselineDataset(AbstractVQADataset):
+	def __init__(self):
+	def __getitem__(self, idx):
+	def __len__(self):
+
+
