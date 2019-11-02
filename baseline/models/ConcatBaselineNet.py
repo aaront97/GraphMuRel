761d5e4e60e19a54349da65558ffa9c7c6996c62
aaront97 <aaron_tjandra@yahoo.com>
2019-11-02 15:13:18 +0000

    added ConcatBaselineNet

--- baseline/models/ConcatBaselineNet.py
+++ baseline/models/ConcatBaselineNet.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class ConcatBaselineNet(nn.Module):
-    def __init__(self, input_dim, out_dim, hidden_list):
+    #Todo Dropout?
+    def __init__(self, input_dim, out_dim, hidden_list, dropout):
         #Hidden list expects a list of arguments that specifies
         #The depth and length of the hidden layers, e.g.
         # [4448, 1024, 512, 3000]
@@ -11,18 +12,23 @@ class ConcatBaselineNet(nn.Module):
         self.input_dim = input_dim
         self.out_dim = out_dim
         self.hidden = nn.ModuleList()
-        if not hidden_list or len(hidden_list) == 1:
-            print("Hidden List is empty! Initialising MLP with one hidden layer")
+        self.dropout = dropout
+        if not hidden_list or len(hidden_list) <= 2:
+            print("Initialising MLP with one hidden layer with no dropout")
             self.hidden.append(nn.Linear(input_dim, out_dim))
         else:
             assert hidden_list[0] == input_dim and hidden_list[-1] == out_dim, \
              "Please make the first and last element equal to input_dim and out_dim respectively"
             for i in range(len(hidden_list) - 1):
                 self.hidden.append(nn.Linear(hidden_list[i], hidden_list[i+1]))
-    
+                self.hidden.append(nn.Dropout(p=self.dropout))
+                
     def forward(self, x):
         for layer in self.hidden:
-            x = F.relu(layer(x))
+            if isinstance(layer, nn.Dropout):
+                x = layer(x)
+            else:
+                x = F.relu(layer(x))
         return x
         
         
