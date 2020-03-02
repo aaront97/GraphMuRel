d188326126c5385727f5710b98c50453343d7d4a
aaront97 <aaron_tjandra@yahoo.com>
2020-03-02 16:41:09 +0000

    added test for constructing and saving graphs

--- tests/serialise_construct_time_test.py
+++ tests/serialise_construct_time_test.py
@@ -12,7 +12,7 @@ def constr_graph(feat_dir, images_list, graph_files_dir):
     constructor = T.KNNGraph(k=6, force_undirected=True)
     res = []
     for name in images_list:
-        feat_path = feat_dir + name
+        feat_path = os.path.join(feat_dir, name)
         dict_feats = torch.load(feat_path)
         data = Data(x=dict_feats['pooled_feat'], pos=dict_feats['norm_rois'])
         res.append(constructor(data))
@@ -22,7 +22,7 @@ def constr_graph(feat_dir, images_list, graph_files_dir):
 def save_graphs(feat_dir, images_list, graph_files_dir):
     constructor = T.KNNGraph(k=6, force_undirected=True)
     for name in images_list:
-        feat_path = feat_dir + name
+        feat_path = os.path.join(feat_dir, name)
         dict_feats = torch.load(feat_path)
         data = Data(x=dict_feats['pooled_feat'], pos=dict_feats['norm_rois'])
         data = constructor(data)
@@ -38,20 +38,24 @@ def load_graphs(feat_dir, images_list, graph_files_dir):
     return res
 
 
+
+feat_dir = '/auto/homes/bat34/2018-04-27_bottom-up-attention_fixed_36'
+graph_files_dir = '/auto/homes/bat34/VQA_PartII/tests/test_graphs'
+no_images = 1000
+
+if not os.path.exists(graph_files_dir):
+    subprocess.run(['mkdir', '-p', graph_files_dir])
+
+images_list = os.listdir(feat_dir)[:no_images]
 def main():
-    feat_dir = '/auto/homes/bat34/2018-04-27_bottom-up-attention_fixed_36'
-    graph_files_dir = '/auto/homes/bat34/VQA_PartII/tests/test_graphs'
-    no_images = 1000
-
-    if not os.path.exists(graph_files_dir):
-        subprocess.run(['mkdir', '-p', graph_files_dir])
-
-    images_list = os.listdir(feat_dir)[:no_images]
-    t = timeit.timeit('constr_graph(feat_dir, images_list, graph_files_dir)')
+    t = timeit.timeit('constr_graph(feat_dir, images_list, graph_files_dir)',
+                      'from __main__ import constr_graph, feat_dir, images_list, graph_files_dir')
     print('Constructing graphs took: {} s'.format(t))
-    t = timeit.timeit('save_graphs(feat_dir, images_list, graph_files_dir)')
+    t = timeit.timeit('save_graphs(feat_dir, images_list, graph_files_dir)', 
+                      'from __main__ import save_graphs, feat_dir, images_list, graph_files_dir')
     print('Saving graphs took: {} s'.format(t))
-    t = timeit.timeit('load_graphs(feat_dir, images_list, graph_files_dir)')
+    t = timeit.timeit('load_graphs(feat_dir, images_list, graph_files_dir)',
+                      'from __main__ import load_graphs, feat_dir, images_list, graph_files_dir')
     print('Loading graphs took" {} s'.format(t))
 
 if __name__ == '__main__':
