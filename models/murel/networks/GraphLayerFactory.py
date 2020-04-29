71b70c5b2a3b5573060bebb7c83a1ebc9aa96c0a
aaront97 <aaron_tjandra@yahoo.com>
2020-04-29 16:16:52 +0100

    added graph layer factory

---
+++ models/murel/networks/GraphLayerFactory.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, GATConv, GraphConv
+
+
+class GraphLayerFactory:
+    def __init__(self):
+        pass
+
+    def get_graph_layer(self, config):
+        if config['graph_layer_type'] == 'gcnconv':
+            return GCNConv
+        elif config['graph_layer_type'] == 'gatconv':
+            return GATConv
+        elif config['graph_layer_type'] == 'GraphConv':
+            return GraphConv
+        else:
+            raise ValueError('Unrecognised graph layer type. Current available layers: GCN, GAT, GraphConv.')
